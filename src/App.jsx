073a3ce0a8ea33f332e1b704f8b16eb542cff4af
philatm <philamooh@gmail.com>
2024-05-11 02:41:25 +0500

    Add color to BMI string respectively to state

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,27 @@ import { useState, useEffect } from "react";
 import UnitInput from "./components/UnitInput/UnitInput";
 import "./App.css";
 
+function getBMIColor(BMI) {
+  if (BMI < 16) {
+    return "#00FFF0";
+  }
+  if (BMI < 18.5) {
+    return "#00FFFF";
+  }
+  if (BMI < 25) {
+    return "#00FF00";
+  }
+  if (BMI < 30) {
+    return "#ADFF2F";
+  }
+  if (BMI < 35) {
+    return "#FFFF00"
+  }
+  if (BMI < 40) {
+    return "#FF8000"
+  }
+  return "#F00000"
+}
 const App = () => {
   const [mass, setMass] = useState(68);
   const [height, setHeight] = useState(170);
@@ -31,7 +52,7 @@ const App = () => {
           min="3.1"
           max="250"
         />
-        <h2>
+        <h2 style={{color: getBMIColor(BMI) }}>
           BMI: {BMI.toFixed(2)} kg/m<sup>2</sup>
         </h2>
       </div>
@@ -47,6 +68,6 @@ const App = () => {
       </button>
     </>
   );
-}
+};
 
 export default App;
